634ae6c08dcc82142b24ea45161d6dee74db6562
Taylor Cooke <taylorcooke@gmail.com>
2019-03-04 20:30:43 -0500

    migrations finally working

--- taylor_django/urls.py
+++ taylor_django/urls.py
@@ -40,15 +40,11 @@ urlpatterns += [
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
-               path('admin/', admin.site.urls),
-               path('catalog/', include('catalog.urls')),
-               path('', RedirectView.as_view(url='/catalog/')),
-               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-from django.urls import path
-from . import views
+# from django.urls import path
+# from . import views
 
-urlpatterns = [
+# urlpatterns = [
 
-]
+# ]
