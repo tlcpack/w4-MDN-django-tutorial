771c5f274c5a0222018d50a0df53c81fdf32a843
Taylor Cooke <taylorcooke@gmail.com>
2019-03-06 20:55:40 -0500

    added password reset

--- taylor_django/urls.py
+++ taylor_django/urls.py
@@ -44,3 +44,7 @@ from django.conf.urls.static import static
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
+#Add Django site authentication urls (for login, logout, password management)
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
